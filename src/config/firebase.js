3bd2297d87393020ae47c9a3a81c67e866496ab1
RyanRanjith <kingranjith4@gmail.com>
2024-09-03 08:21:22 -0700

    a

---
+++ src/config/firebase.js
@@ -0,0 +1,63 @@
+
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { doc, getFirestore, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD4wNWqL-be0bBwmE9kAegxk-6z4dSNoOY",
+  authDomain: "chat-app-gs-16ca6.firebaseapp.com",
+  projectId: "chat-app-gs-16ca6",
+  storageBucket: "chat-app-gs-16ca6.appspot.com",
+  messagingSenderId: "514402674175",
+  appId: "1:514402674175:web:3b534ebbe1ced7d91bf3ca"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username,email,password) => {
+     try {
+        const res = await createUserWithEmailAndPassword(auth,email,password);
+        const user = res.user;
+        await setDoc(doc(db,"users",user.uid),{
+            id:user.uid,
+            username:username.toLowerCase(),
+            email,
+            name:"",
+            avatar:"",
+            bio:"Hey, There i am using chat app",
+            lastSeen:Date.now()
+
+        })
+        await setDoc(doc(db,"chats",user.uid),{
+            chatData:[]
+        })
+     } catch (error) {
+        console.error(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+     }
+}
+
+const login = async (email,password) => {
+    try {
+      await signInWithEmailAndPassword(auth,email,password)
+    } catch (error) {
+      console.error(error);
+      toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const logout = async () => {
+  try {
+    await signOut(auth)
+  } catch (error) {
+    console.error(error);
+      toast.error(error.code.split('/')[1].split('-').join(" "));
+  }
+ 
+}
+
+export {signup,login,logout,auth,db}
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,19 +1,35 @@
 import React, { useState } from 'react'
 import './Login.css'
 import assets from '../../assets/assets'
+import { signup, login } from '../../config/firebase'
 
 const Login = () => {
 
    const [currState,setCurrState] = useState("Sign up");
+   const [userName,setUserName] = useState("");
+   const [email,setEmail] = useState("");
+   const [password,setPassword] = useState("");
+
+   const onSubmitHandler = (event) => {
+    event.preventDefault();
+    if (currState === "Sign up") {
+      signup(userName,email,password);
+    }
+    else{
+      login(email,password)
+    }
+
+   }
+
 
   return (
     <div className='login'>
      <img src={assets.logo_big}  className="logo" alt="" />
-       <form  className='login-form'>
+       <form onSubmit={onSubmitHandler}  className='login-form'>
         <h2>{currState}</h2>
-        {currState === "Sign up"? <input type="text" placeholder='Username' className="form-input" required />:null}
-        <input type="email" placeholder='Email Address' className="form-input" required />
-        <input type="password" placeholder='password' className="form-input" required/>
+        {currState === "Sign up"? <input onChange={(e)=>setUserName(e.target.value)} value={userName} type="text" placeholder='Username' className="form-input" required />:null}
+        <input onChange={(e)=>setEmail(e.target.value)} value={email} type="email" placeholder='Email Address' className="form-input" required />
+        <input onChange={(e)=>setPassword(e.target.value)} value={password} type="password" placeholder='password' className="form-input" required/>
         <button type='submit'>{currState === "Sign up"?"Create account":"Login now"}</button>
         <div className="login-term">
           <input type="checkbox" />
