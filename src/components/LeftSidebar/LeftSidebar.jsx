f9b7577aea071305f0d03b24e0bf685bcde93103
RyanRanjith <kingranjith4@gmail.com>
2024-09-06 10:55:22 -0700

    a

--- src/components/LeftSidebar/LeftSidebar.jsx
+++ src/components/LeftSidebar/LeftSidebar.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import './LeftSidebar.css'
 import assets from '../../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { collection, getDocs, query, where } from 'firebase/firestore'
+import{db} from '../../config/firebase'
+import { AppContext } from '../../context/AppContext'
   
 const LeftSidebar = () => {
 
   const navigate = useNavigate();
+  const {userData} = useContext(AppContext);
+  const [user,setUser] = useState(null);
+  const [showSearch,setShowSearch] = useState(false);
+
+  const inputHandler = async (e) => {
+    try {
+      const input = e.target.value;
+      if (input) {
+        setShowSearch(true);
+        const userRef = collection(db,'users');
+        const q = query(userRef,where("username","==",input.toLowerCase()));
+        const querySnap = await getDocs(q);
+        if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
+          setUser(querySnap.docs[0].data());
+          
+        }
+        else{
+          setUser(null);
+        }
+      }
+      else{
+        setShowSearch(false);
+      }
+    
+    } catch (error) {
+      
+    }
+  }
 
   return (
     <div className='ls'>
@@ -23,19 +54,26 @@ const LeftSidebar = () => {
             </div>
             <div className="ls-search">
                 <img src={assets.search_icon} alt="" />
-                <input type="text" placeholder='Search Here' />
+                <input onChange={inputHandler} type="text" placeholder='Search Here' />
             </div>
         </div>
       <div className="ls-list">
-       {Array(12).fill("").map((item,index)=>(
-          <div key={index} className="friends">
-          <img src={assets.profile_img} alt="" />
-          <div>
-              <p>Richard Sanford</p>
-              <span>Hello, How are you?</span>
-          </div>
-         </div>
-        ))}
+      {showSearch && user
+      ? <div className='friends add-user'>
+          <img src={user.avatar} alt=""/>
+          <p>{user.name}</p>   
+      </div>
+      :Array(12).fill("").map((item,index)=>(
+        <div key={index} className="friends">
+        <img src={assets.profile_img} alt="" />
+        <div>
+            <p>Richard Sanford</p>
+            <span>Hello, How are you?</span>
+        </div>
+       </div>
+      ))
+      }
+       
       </div>
     </div>
   )
