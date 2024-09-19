280881c6f4635f84e6d0870bc85f570a95b641fe
RyanRanjith <kingranjith4@gmail.com>
2024-09-19 10:33:58 -0700

    a

--- src/components/ChatBox/ChatBox.jsx
+++ src/components/ChatBox/ChatBox.jsx
@@ -5,6 +5,7 @@ import { AppContext } from "../../context/AppContext";
 import { arrayUnion, doc, getDoc, onSnapshot, updateDoc } from "firebase/firestore";
 import { db } from "../../config/firebase";
 import { toast } from "react-toastify";
+import upload from '../../lib/upload'
 
 const ChatBox = () => {
 
@@ -47,6 +48,57 @@ const ChatBox = () => {
     setInput("");
   }
 
+  const sendImage = async (e) => {
+    try {
+      const fileUrl = await upload(e.target.files[0]);
+        
+      if (fileUrl && messagesId) {
+        await updateDoc(doc(db,'messages',messagesId),{
+          messages: arrayUnion({
+            sId:userData.id,
+            image:fileUrl,
+            createdAt:new Date()
+          })
+        })
+
+        const userIDs = [chatUser.rId,userData.id];
+
+        userIDs.forEach(async (id) => {
+          const userChatsRef = doc(db,'chats',id);
+          const userChatsSnapshot = await getDoc(userChatsRef);
+
+          if (userChatsSnapshot.exists()) {
+            const userChatData = userChatsSnapshot.data();
+            const chatIndex = userChatData.chatsData.findIndex((c)=>c.messageId === messagesId);
+            userChatData.chatsData[chatIndex].lastMessage = "Image";
+            userChatData.chatsData[chatIndex].updatedAt = Date.now();
+            if (userChatData.chatsData[chatIndex].rId === userData.id) {
+              userChatData.chatsData[chatIndex].messageSeen = false;
+            }
+            await updateDoc(userChatsRef,{
+              chatsData:userChatData.chatsData
+            })
+          }
+        })
+      }
+
+    } catch (error) {
+      toast.error(error.message)
+    }
+  }
+
+  const convertTimestamp = (timestamp) => {
+    let date = timestamp.toDate();
+    const hour = date.getHours();
+    const minute = date.getMinutes();
+    if (hour>12) {
+      return hour - 12 + ":" + minute + " PM";
+    }
+    else{
+      return hour +  ":" + minute + " AM";
+    }
+  }
+
   useEffect(()=>{
    if (messagesId) {
     const unSub = onSnapshot(doc(db,'messages',messagesId),(res)=>{
@@ -74,38 +126,24 @@ const ChatBox = () => {
       <div className="chat-msg">
 
         {messages.map((msg,index)=>(
-           <div className="s-msg">
-           <p className="msg">
-             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia{" "}
-           </p>
+           <div key={index} className={msg.sId === userData.id ? "s-msg" : "r-msg"}>
+            {msg["image"]
+            ? <img className="msg-img" src={msg.image} alt="" />
+            :<p className="msg">{msg.text}</p>
+            }
            <div className="">
-             <img src={assets.profile_img} alt="" />
-             <p>2:30 PM</p>
+             <img src={msg.sId === userData.id ? userData.avatar : chatUser.userData.avatar} alt="" />
+             <p>{convertTimestamp(msg.createdAt)}</p>
            </div>
          </div>
         ))}
        
-        <div className="s-msg">
-          <img className="msg-img" src={assets.pic1} alt="" />
-          <div className="">
-            <img src={assets.profile_img} alt="" />
-            <p>2:30 PM</p>
-          </div>
-        </div>
-        <div className="r-msg">
-          <p className="msg">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia{" "}
-          </p>
-          <div className="">
-            <img src={assets.profile_img} alt="" />
-            <p>2:30 PM</p>
-          </div>
-        </div>
+       
       </div>
 
       <div className="chat-input">
         <input onChange={(e)=>setInput(e.target.value)} value={input} type="text" placeholder="Send a Message" />
-        <input type="file" id="image" accept="image/png, image/jpeg" hidden />
+        <input onChange={sendImage} type="file" id="image" accept="image/png, image/jpeg" hidden />
         <label htmlFor="image">
           <img src={assets.gallery_icon} alt="" />
         </label>
